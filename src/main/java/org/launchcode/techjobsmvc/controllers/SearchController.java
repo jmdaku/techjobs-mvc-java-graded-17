bc9e1221478f0edb99c89e406a82b54404f13d9d
jmdaku <jmdaku@gmail.com>
2023-10-17 16:07:45 -0400

    completed todo#2: added view all

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,14 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +27,29 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    @PostMapping("/results")
+    public ArrayList<Job> displaySearchResults(String searchTerm, String searchType, Model model){
+        if(searchTerm.equals("all")) {
+
+            return JobData.findAll();
+
+        }else if(searchTerm.isEmpty()){
+
+            return JobData.findAll();
+
+        }else{
+            return JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+    }
+
+
+//    In order for these last two parameters to be properly passed in by Spring Boot, you need to use the correct annotation.
+//    Pass jobs into the search.html view via the model parameter.
+//    Pass ListController.columnChoices into the view, as the existing search handler does.
+
+
+
 
 }
 
