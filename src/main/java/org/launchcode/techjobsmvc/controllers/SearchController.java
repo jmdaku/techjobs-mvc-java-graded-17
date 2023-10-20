202155957f4c8f346043c7adb36ca582d4d2a628
jmdaku <jmdaku@gmail.com>
2023-10-19 20:03:35 -0400

    completed todo #1 - fixed Position Type

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -5,10 +5,12 @@ import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -28,20 +30,22 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-    @PostMapping("/results")
-    public ArrayList<Job> displaySearchResults(String searchTerm, String searchType, Model model){
-        if(searchTerm.equals("all")) {
-
-            return JobData.findAll();
-
-        }else if(searchTerm.isEmpty()){
-
-            return JobData.findAll();
-
-        }else{
-            return JobData.findByColumnAndValue(searchType, searchTerm);
-        }
-    }
+//    @PostMapping("/results")
+//    public String displaySearchResults(@ModelAttribute searchTerm, String searchType, Model model){
+//        List<Job> jobs= new ArrayList<>();
+//
+//        if(searchTerm.equals("all")) {
+//            model.addAttribute();
+//            jobs.add(JobData.findAll());
+//
+//        }else if(searchTerm.isEmpty()){
+//            JobData.findAll();
+//
+//        }else{
+//            JobData.findByColumnAndValue(searchTerm, searchType);
+//        }
+//        return "/search/results";
+//    }
 
 
 //    In order for these last two parameters to be properly passed in by Spring Boot, you need to use the correct annotation.
